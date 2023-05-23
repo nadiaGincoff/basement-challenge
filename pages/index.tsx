32f556c233a366a055942df1018c2784b7c0d8c7
nadiaGincoff <nadia.gincoff@gmail.com>
2023-05-23 21:31:02 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
 import type {NextPage} from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "../public/logo.svg";
+import header from "../public/header.svg";
+import iconHeaderCenter from "../public/iconHeaderCenter.svg";
+import iconHeaderCenter1 from "../public/iconHeaderCenter1.svg";
+import iconHeaderCenter2 from "../public/iconHeaderCenter2.svg";
+import iconHeaderCenter3 from "../public/iconHeaderCenter3.svg";
+import iconHeaderCenter4 from "../public/iconHeaderCenter4.svg";
+
+const elementsInCart = 0;
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="max-w-full flex items-center justify-between p-32px">
+        <Link href={"/"}>
+          <a>
+            <Image alt="logo" height="28.15px" src={logo} width="192px" />
+          </a>
+        </Link>
+        <div>
+          <Image alt="iconHeaderCenter" src={iconHeaderCenter} />
+          <Image alt="iconHeaderCenter" src={iconHeaderCenter1} />
+          <Image alt="iconHeaderCenter" src={iconHeaderCenter2} />
+          <Image alt="iconHeaderCenter" src={iconHeaderCenter3} />
+          <Image alt="iconHeaderCenter" src={iconHeaderCenter4} />
+        </div>
+        <button className="inline-flex items-center rounded-large border-white border-1 py-13px px-32px">
+          <p className="leading-4">CART({elementsInCart})</p>
+        </button>
+      </div>
+      <Image alt="logo" src={header} />
+    </header>
+  );
+};
 
 const Home: NextPage = () => {
   return (
-    <div className="h-full flex bg-black">
-      <header className="m-auto text-white text-center">
-        <Image alt="Basement" src={logo} />
-        <h4>Lets get this party started</h4>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      DEFAULT: "4px",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "56px",
+    },
+    borderWidth: {
+      DEFAULT: "1.5px",
+      0: "0",
+      1: "1.5px",
+      2: "2px",
+    },
+    padding: {
+      "32px": "32px",
+      "13px": "13px",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
